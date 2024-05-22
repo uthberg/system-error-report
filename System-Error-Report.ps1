3cced43386f50fc21776cfa1631e02e38f680e15
Matt <115906976+uthberg@users.noreply.github.com>
2024-05-22 16:31:24 -0700

    Update System-Error-Report.ps1

--- System-Error-Report.ps1
+++ System-Error-Report.ps1
@@ -1,6 +1,11 @@
 #displays all active entry logs
 Get-EventLog -List
+
 #lists current powershell execution policy
 Get-ExecutionPolicy -List
+
 #lists all discovered system file erros
 Get-EventLog -LogName System -EntryType Error
+
+#lists all successful audits accessed
+Get-EventLog -LogName Security -EntryType SuccessAudit
